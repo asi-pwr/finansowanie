a561d94ff5ae0af5c6618fafc9c5c3101d3bf0eb
Michał Kochański <m.e.kochanski@gmail.com>
2017-06-08 20:35:40 +0200

    Add missing validations to all previous models

--- app/models/project_member.rb
+++ app/models/project_member.rb
@@ -2,4 +2,5 @@ class ProjectMember < ApplicationRecord
   belongs_to :application
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :function, presence: true
 end
--- app/models/schedule_item.rb
+++ app/models/schedule_item.rb
@@ -1,4 +1,8 @@
 class ScheduleItem < ApplicationRecord
   belongs_to :application
   validates :todo, presence: true
+  validates :start_date, presence: true
+  validates :due_date, presence: true
+  validates :finance_source, presence: true
+  validates :budget_amount, presence: true
 end
