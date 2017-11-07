4d27a524b135d384f6b4c0909d0975ae35256c4d
Jan Librowski <janlibrowski@gmail.com>
2017-11-08 00:14:50 +0100

    Modify users table

---
+++ db/migrate/20171107230916_change_users.rb
@@ -0,0 +1,9 @@
+class ChangeUsers < ActiveRecord::Migration[5.1]
+  def change
+    change_table :users do |t|
+      t.remove :has_training
+      t.references :faculty, foreign_key: true
+      t.string :phone_number
+    end
+  end
+end
