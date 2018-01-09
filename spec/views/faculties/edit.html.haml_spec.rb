f2732af131479993e81f82a4fa0576e9fa693ccb
MichalKarbownik <michakarbownik@gmail.com>
2018-01-09 21:39:39 +0100

    Controller scaffold generated

---
+++ spec/views/faculties/edit.html.haml_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "faculties/edit", type: :view do
+  before(:each) do
+    @faculty = assign(:faculty, Faculty.create!())
+  end
+
+  it "renders the edit faculty form" do
+    render
+
+    assert_select "form[action=?][method=?]", faculty_path(@faculty), "post" do
+    end
+  end
+end
