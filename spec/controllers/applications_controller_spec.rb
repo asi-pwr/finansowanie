bdb80f0b9cf3d3e9ab62a2399c6b10015d3c8bc2
Tomasz Zalupski <tzalupski@gmail.com>
2018-01-23 20:40:00 +0100

    Add tests for applications controller

--- spec/controllers/applications_controller_spec.rb
+++ spec/controllers/applications_controller_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 RSpec.describe ApplicationsController, type: :controller do
 
+  it { should use_before_action(:authenticate_user!) }
+
+  it do
+    params = {
+      application: {
+                     name: 'ASI',
+                     coordinator: 'Seba',
+                     amount_applied_for: 21,
+                     amount_other_sources: 37,
+                     amount_overall: 58
+                   }
+             }
+             should permit(:name, :coordinator, :amount_applied_for, :amount_other_sources, :amount_overall).
+               for(:create, params: params).
+               on(:application)
+  end  
+
   describe "GET #new" do
     it "returns http success" do
       get :new
