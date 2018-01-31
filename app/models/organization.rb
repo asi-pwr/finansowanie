55f1c081a77ba0d72cb8456a56c914b23c8e6199
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2018-01-31 20:04:36 +0100

    Restore associations to working state - rubocop still complains about unspecified :inverse_of relations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,7 +3,7 @@
 class Organization < ApplicationRecord
   belongs_to :faculty
   has_many :user_organizations, dependent: :destroy
-  has_many :users, through: :user_organizations, inverse_of: :organization, dependent: :destroy
+  has_many :users, through: :user_organizations, dependent: :destroy
   has_many :applications, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :time_of_establishment, presence: true
