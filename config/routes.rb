ccb0d00c9bb7b76ee8ab2d65124a19aa5ff4ec6c
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2018-10-16 18:38:54 +0200

    Update deprecated gems (therubyracer -> mini_racer)

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@
 
 Rails.application.routes.draw do
   resources :organizations
-<<<<<<< HEAD
+#HEAD
   resources :applications, only: %i[new create]
   root 'pages#home'
   get "/", to: "pages#home"
-=======
-  resources :applications #, only: %i[index new create]
-  root 'pages#root'
-  get "/root", to: "pages#root"
->>>>>>> Add basic UI for viewing applications
+# Add basic UI for viewing applications
   devise_for :users
   resources :faculties
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
