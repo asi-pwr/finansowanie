a0133380662ad426ddcb0563a747293d50b02ba8
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2019-02-06 12:54:13 +0100

    Organization spec passes now

--- spec/factories/schedule_items.rb
+++ spec/factories/schedule_items.rb
@@ -2,11 +2,9 @@
 
 FactoryBot.define do
   factory :schedule_item do
+    application
     todo { "MyString" }
     start_date { "2017-06-08" }
     due_date { "2017-06-08" }
-    finance_source { "MyString" }
-    budget_amount { 1 }
-    application { nil }
   end
 end
