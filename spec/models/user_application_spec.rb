8ccdb3a20524c953d753136dde2f0190148290a1
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2019-02-06 13:57:41 +0100

    Add user application factory and spec

--- spec/models/user_application_spec.rb
+++ spec/models/user_application_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UserApplication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user_application)).to be_valid
+  end
 end
