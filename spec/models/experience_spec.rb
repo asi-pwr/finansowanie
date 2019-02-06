b775f5acd168ecc5ff9489774b56c129d3b51652
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2019-02-06 13:26:08 +0100

    Add experience factory and spec

--- spec/models/experience_spec.rb
+++ spec/models/experience_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Experience, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to validate_presence_of(:project_name) }
+  it { is_expected.to validate_presence_of(:role) }
+  it { is_expected.to validate_presence_of(:project_date) }
+
+  it { is_expected.to validate_presence_of(:budget) }
+  it { is_expected.to validate_numericality_of(:budget).is_greater_than_or_equal_to(0) }
+
+  it { is_expected.to validate_presence_of(:member_name) }
+
+  it "has a valid factory" do
+   expect(FactoryBot.build(:experience)).to be_valid
+ end 
+
 end
