278aa52be56e926eb8050d8a0aaceb1cf7ceac70
TeachMeHow <szymoncichy123@gmail.com>
2019-02-19 16:26:19 +0100

    all model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true, uniqueness: true, case_sensitive: false,
-    format: {with: URI::MailTo::EMAIL_REGEXP }
+    format: {with: Devise::email_regexp }
   validates :phone_number, format: {with: /\A[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*\z/}, allow_blank: true
   validates :full_name, presence: true
   has_many :user_applications, dependent: :destroy
--- spec/models/schedule_item_spec.rb
+++ spec/models/schedule_item_spec.rb
@@ -3,10 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe ScheduleItem, type: :model do
+  # tests for built in validators
   it { is_expected.to validate_presence_of(:todo) }
   it { is_expected.to validate_presence_of(:start_date) }
   it { is_expected.to validate_presence_of(:due_date) }
 
+  # tests for associations
+  it { is_expected.to belong_to(:application) }
+
+  # tests for factory
   it "has a valid factory" do
     expect(FactoryBot.build(:schedule_item)).to be_valid
   end
