966f11014dc46f44856e809d02a7947d310fcb6d
CezaryKret <czarekkret@gmail.com>
2019-12-09 21:30:58 +0100

    nested forms prototype

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -30,6 +30,7 @@ class ApplicationsController < ApplicationController
     3.times { @application.experiences.build }
     3.times { @application.schedule_items.build }
     3.times { @application.roles.build }
+    
   end
 
   def create
--- app/webpack/controllers/hello_controller.js
+++ app/webpack/controllers/hello_controller.js
@@ -13,6 +13,6 @@ export default class extends Controller {
   static targets = [ "output" ]
 
   connect() {
-    this.outputTarget.textContent = 'elo elo'
+    this.outputTarget.textContent = 'Hello, Stimulus!'
   }
 }
